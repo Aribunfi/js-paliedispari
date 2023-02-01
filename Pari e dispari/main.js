25631626b78836fdc967d2c2f93b5d633c9e22b7
Aribunfi <arianna@pabis.de>
2023-02-01 15:02:58 +0100

    correzioni e console.log

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,10 +1,14 @@
 // L'utente sceglie pari o dispari 
 
-const userChoice = parseInt(prompt ("Scrivi 'pari' o 'dispari'"));
+const userChoice = prompt ("Scrivi 'pari' o 'dispari'");
+
+    console.log(userChoice);
 
 //e inserisce un numero da 1 a 5.
 
 const userNumber = parseInt(prompt ("Inserisci un numero da 1 a 5"));
+
+    console.log(userNumber);
 
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -14,13 +18,16 @@ const pcNumber = randomPcNumber(1,5);
 
 
 function randomPcNumber (min, max) {
-    const randomNumber = Math.floor(Math.random()*(massimo - minimo +1)) + minimo;
+    const pcNumber = Math.floor(Math.random()*(max - min +1)) + min;
 
-    return randomNumber;
+    console.log(pcNumber);
+
+    return pcNumber;
+
 }
 // Sommiamo i due numeri
 
-let gameTotal = userNumber + randomPcNumber; 
+let gameTotal = userNumber + pcNumber; 
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
@@ -28,9 +35,10 @@ let evenGameTotal = isEven(gameTotal);
 
 function isEven(num) {
     if(num % 2 == 0) {
+        console.log ("La somma dei due numeri è pari");
         return true;
     }
-
+        console.log ("La somma dei due numeri è dispari");
     return false;
 }
 
